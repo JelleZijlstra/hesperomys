34d2660fb43b58fc4763715bbdaa186ca298a8b4
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-01-27 17:45:29 -0800

    show IssueDate

--- src/body/CitationGroupBody.tsx
+++ src/body/CitationGroupBody.tsx
@@ -9,6 +9,7 @@ import CitationGroupArticleSet from "../lists/CitationGroupArticleSet";
 import CitationGroupRedirects from "../lists/CitationGroupRedirects";
 import ModelLink from "../components/ModelLink";
 import Table from "../components/Table";
+import PublicationDate from "./PublicationDate";
 
 const CitationGroupTags = ({
   citationGroup,
@@ -65,6 +66,114 @@ const CitationGroupTags = ({
   );
 };
 
+const IssueDate = ({
+  issueDate,
+  hasSeries,
+  hasIssue,
+}: {
+  issueDate: Exclude<
+    Exclude<
+      Exclude<CitationGroupBody_citationGroup["issuedateSet"], null>["edges"][0],
+      null
+    >["node"],
+    null
+  >;
+  hasSeries: boolean;
+  hasIssue: boolean;
+}) => {
+  return (
+    <tr>
+      {hasSeries && <td>{issueDate.series || ""}</td>}
+      <td>{issueDate.volume}</td>
+      {hasIssue && <td>{issueDate.issue || ""}</td>}
+      <td>
+        {issueDate.startPage}–{issueDate.endPage}
+      </td>
+      <td>
+        <PublicationDate date={issueDate.date} />
+      </td>
+      <td>
+        {issueDate.tags && (
+          <ul>
+            {issueDate.tags.map(
+              (tag) =>
+                tag.__typename === "CommentIssueDate" && (
+                  <li>
+                    {tag.text}{" "}
+                    <small>
+                      <ModelLink model={tag.source} />
+                    </small>
+                  </li>
+                )
+            )}
+          </ul>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+const IssueDates = ({
+  citationGroup,
+}: {
+  citationGroup: CitationGroupBody_citationGroup;
+}) => {
+  if (!citationGroup.issuedateSet || citationGroup.issuedateSet.edges.length === 0) {
+    return null;
+  }
+  const issueDates = citationGroup.issuedateSet.edges
+    .map((edge) => edge?.node)
+    .filter((date) => date !== null && date !== undefined);
+  issueDates.sort((a, b) => {
+    if (a === b) {
+      return 0;
+    }
+    if (!a) {
+      return -1;
+    }
+    if (!b) {
+      return 1;
+    }
+    if (a.date < b.date) {
+      return -1;
+    }
+    if (a.date === b.date) {
+      return 0;
+    }
+    return 1;
+  });
+  const hasSeries = issueDates.some((issueDate) => issueDate?.series);
+  const hasIssue = issueDates.some((issueDate) => issueDate?.issue);
+  return (
+    <>
+      <h3>Issue publication dates</h3>
+      <table className="bordered">
+        <thead>
+          <tr>
+            {hasSeries && <th>Series</th>}
+            <th>Volume</th>
+            {hasIssue && <th>Issue</th>}
+            <th>Pages</th>
+            <th>Date</th>
+            <th>Comments</th>
+          </tr>
+        </thead>
+        {issueDates.map(
+          (issueDate) =>
+            issueDate && (
+              <IssueDate
+                key={issueDate.id}
+                hasSeries={hasSeries}
+                hasIssue={hasIssue}
+                issueDate={issueDate}
+              />
+            )
+        )}
+      </table>
+    </>
+  );
+};
+
 class CitationGroupBody extends React.Component<{
   citationGroup: CitationGroupBody_citationGroup;
 }> {
@@ -78,6 +187,7 @@ class CitationGroupBody extends React.Component<{
           </p>
         )}
         <CitationGroupTags citationGroup={citationGroup} />
+        <IssueDates citationGroup={citationGroup} />
         <CitationGroupArticleSet citationGroup={citationGroup} title="Publications" />
         <CitationGroupNames
           citationGroup={citationGroup}
@@ -117,6 +227,28 @@ export default createFragmentContainer(CitationGroupBody, {
         ... on YearRange {
           start
           end
+        }
+      }
+      issuedateSet(first: 1000) {
+        edges {
+          node {
+            id
+            series
+            volume
+            issue
+            startPage
+            endPage
+            date
+            tags {
+              __typename
+              ... on CommentIssueDate {
+                text
+                source {
+                  ...ModelLink_model
+                }
+              }
+            }
+          }
         }
       }
       ...CitationGroupRedirects_citationGroup
